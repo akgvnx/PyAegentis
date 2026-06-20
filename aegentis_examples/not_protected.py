1aa43d7c9f9501f99c8b2fc2b97ed2bacb5d2450
akgvnx <deneme0340w@gmail.com>
2026-06-20 13:49:33 +0300

    upload - 20/6/2026

---
+++ aegentis_examples/not_protected.py
@@ -0,0 +1,5 @@
+print("Aegentis Protector / PyAegentis - Example App")
+
+if __name__ == "__main__":
+    secret = 40 + 2
+    print(f"Answer: {secret}")
---
+++ aegentis_examples/protected_with_pyaegentis.py
@@ -0,0 +1,3 @@
+from di3n9k7z import tqLvg0xykr2G
+if __name__ == '__main__':
+    tqLvg0xykr2G()
